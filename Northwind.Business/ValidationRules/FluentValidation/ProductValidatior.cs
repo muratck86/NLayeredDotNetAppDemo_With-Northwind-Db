7fb70ee0c2e3cb0f4972586b9c12d4f788b23783
Murat Can Kurt <kurt.muratcan@gmail.com>
2023-08-04 22:54:10 +0300

    Added a FluentValidation class with some examples of how to define validation rules.

---
+++ Northwind.Business/ValidationRules/FluentValidation/ProductValidatior.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Business.ValidationRules.FluentValidation
+{
+    public class ProductValidatior : AbstractValidator<Product>
+    {
+        public ProductValidatior()
+        {
+            RuleFor(p => p.ProductName).NotEmpty();
+            RuleFor(p => p.CategoryId).NotEmpty().WithMessage("Kategori boş olamaz.");
+            RuleFor(p => p.UnitPrice).NotEmpty();
+            RuleFor(p => p.UnitsInStock).NotEmpty().GreaterThanOrEqualTo((short)0);
+            RuleFor(p => p.UnitPrice).GreaterThan(0);
+
+            RuleFor(p => p.UnitPrice).GreaterThan(10).When(p => p.CategoryId == 2);
+
+            RuleFor(p => p.ProductName).Must(StartsWithA);
+
+        }
+
+        private bool StartsWithA(string arg)
+        {
+            return arg.StartsWith("A") || arg.StartsWith("a");
+        }
+    }
+}
